afb1bca8bb5690e792228288a3d2292e6259da22
김정현 <kr100475@gmail.com>
2023-12-27 16:54:02 +0900

    initial commit

--- Front/enjoytrip/src/components/user/UserLogin.vue
+++ Front/enjoytrip/src/components/user/UserLogin.vue
@@ -62,7 +62,7 @@ const login = async () => {
                     <div>
                         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">아이디</label>
                         <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required=""
+                            <input id="email" name="email" type="text" autocomplete="email" required=""
                                 v-model="loginUser.userId"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         </div>
--- Front/enjoytrip/src/components/user/regist.vue
+++ Front/enjoytrip/src/components/user/regist.vue
@@ -116,7 +116,7 @@ function moveMain() {
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">이름</label>
                     <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required=""
+                        <input id="email" name="email" type="text" autocomplete="email" required=""
                             v-model="userInfo.userName"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
